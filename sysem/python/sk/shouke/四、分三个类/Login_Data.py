177d4e1263bc62ef27c3c8c14cec44221c9edf12
xingl <dreamboat0116@126.com>
2024-06-12 19:51:47 +0800

    first

---
+++ sysem/python/sk/shouke/四、分三个类/Login_Data.py
@@ -0,0 +1,90 @@
+import yaml
+import openpyxl
+import pymysql
+
+
+class Login_data:
+    Login_data=[
+        ["lisi","123456",'个人理财系统']
+    ]
+    NameErrorData=[
+        ["lisi", "12347756", '个人理财系统']
+    ]
+# excel 以下——————————————————————————————————————————————————————————————————————————————————————————————————————
+    def login_success_data(self,filename,sheet_name=None):#1个用于读取excel文件的函数，参数代表：要读取的sheet页，默认None
+        data = []
+        #通过openpyxl模块加载excel，给定路径
+        wb = openpyxl.load_workbook(filename=filename)
+        #判断参数是不是空值，是的话就第1个
+        if sheet_name is None:
+            sheet_name = wb.sheetnames[0]  # 默认读取第一个 sheet
+        st = wb[sheet_name]#获取到实际的sheet页
+
+        #opebpyxl中的 st.max_row 最大列数，st.max_column最大行数
+        for i in range(2, st.max_row + 1):#，左闭右开，从第2行开始遍历到最后1行，
+            da = []
+            for j in range(1, st.max_column):#从第2列开始遍历到最后1列
+                cell_value = st.cell(row=i, column=j).value#获取当前行和列的值
+                da.append(cell_value)#把每个单元格中的数据放入da列表
+            data.append(da)#再将ta放入data列表
+        return data
+
+#以下yaml——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+    def yamldata(self,file_path):
+
+        with open(file_path, "r", encoding="utf-8") as file:
+            yaml_data = yaml.safe_load(file)
+
+            return yaml_data
+#以下db——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+
+    def execute_query(self,host, user, password, database, query, params=None, charset = 'utf8mb4', port = '3306'):
+
+        db_config = {
+            'host': host,
+            'user': user,
+            'password': password,
+            # 'database': database,
+            'charset': charset,
+            'port': port
+        }
+        connection = None
+        result = None
+
+        try:
+            connection = pymysql.connect(**db_config)
+
+            with connection.cursor() as cursor:
+                cursor.execute(query, params)
+
+                result = cursor.fetchall()
+
+            connection.commit()
+
+        except Exception as e:
+            print(f"Error: {e}")
+
+        finally:
+            if connection:
+                connection.close()
+
+        return result
+
+
+
+if __name__ == '__main__':
+    login_data = Login_data()
+    login_data.data()
+    # data = login_data.loginSuccessDtate()'
+    host = 'your_host'
+    user = 'your_user'
+    password = 'your_password'
+    database = 'your_database'
+    query = 'SELECT * FROM your_table WHERE column = %s'
+    params = ('value',)
+
+    results = execute_query(host, user, password, database, query, params)
+    for row in results:
+        print(row)
+    #----------------------------------------------------------------------------------------------
